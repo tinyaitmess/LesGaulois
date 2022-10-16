cd9a0b1d22e6bc1bc2d653016a1cee196d876da8
tinyaitmess <tinhinane.ait-messaoud@univ-tlse3.fr>
2022-10-16 23:06:06 +0200

    Update Village.java

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -5,13 +5,19 @@ public class Village {
 	private Chef chef;
 	private int nbVillageoisMaximum=0;
 	private Gaulois[] villageois;
-	private int nbVillageois;
+	private int nbVillageois=0;
 	
 	
-	public Village(int nbVillageoisMaximum) {
+	public Village(String nom,int nbVillageoisMaximum) {
 		super();
 		Gaulois [] villageois = new Gaulois[nbVillageoisMaximum];
 		this.nbVillageois = nbVillageoisMaximum;
+	}
+
+
+
+	public void setVillageois(Gaulois[] villageois) {
+		this.villageois = villageois;
 	}
 
 
@@ -22,6 +28,10 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
+	public Chef getChef() {
+		return chef;
+	}
+
 
 	public String getNom() {
 		return nom;
@@ -36,8 +46,29 @@ public class Village {
 		return villageois[numeroVillageois];
 	}
 	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef"+chef.getNom()+"vivent les légendaires"
+							+"gaulois :");
+	}
+	
 	public static void main (String[] args) {
-		Village village = new Village ("Village des Irr�ductibles",30);
+		Village village = new Village ("Village des Irréductibles",30);
+//		Gaulois gaulois = village.trouverHabitant(30);
+		
+		Chef chef = new Chef ("Abraracourcix ",6,1,village) ;
+		Gaulois asterix = new Gaulois("Astérix",8);
+		village.ajouterHabitant(asterix);
+		
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//		Exception in thread "main" java.lang.NullPointerException: Cannot store to object array because "this.villageois" is null
+//		at personnages.Village.ajouterHabitant(Village.java:31)
+//		at personnages.Village.main(Village.java:44)
+		Gaulois obelix = new Gaulois ("Obélix",25);
+		village.ajouterHabitant(obelix);
+		
+		
+		
 	}
 	
 }
